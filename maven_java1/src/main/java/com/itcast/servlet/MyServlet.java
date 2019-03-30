009ff8501f8c12a70a38ce8eca995d06fbcaa8fd
1064030249 <1064030249@qq.com>
2019-03-30 17:58:53 +0800

    update

---
+++ maven_java1/src/main/java/com/itcast/servlet/MyServlet.java
@@ -0,0 +1,23 @@
+package com.itcast.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 10640
+ * Date: 2019-03-30
+ * Time: 15:57
+ */
+public class MyServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        request.getRequestDispatcher("/hello.jsp").forward(request,response);
+    }
+}
---
+++ maven_java1/src/main/webapp/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 10640
+  Date: 2019/3/30
+  Time: 16:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+  hello
+</body>
+</html>
