0a2a5e178bc0117754a6ad428242f6cf59a4e262
1064030249 <1064030249@qq.com>
2019-05-15 19:19:02 +0800

    update

---
+++ struts2Demo01/src/com/xp/HelloAction.java
@@ -0,0 +1,15 @@
+package com.xp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 10640
+ * Date: 2019-05-15
+ * Time: 19:06
+ */
+public class HelloAction {
+    public String sayHello(){
+        System.out.println("have success");
+        return "success";
+    }
+}
